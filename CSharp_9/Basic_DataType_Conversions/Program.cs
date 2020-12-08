e344bdff30025dd6d9b21c01c7ea18a517dd3590
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2020-12-08 23:53:52 +0400

    basic type conversion

---
+++ CSharp_9/Basic_DataType_Conversions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Basic_DataType_Conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                byte num1 = 3;
+                Console.WriteLine(num1);
+                int numShort = num1 + 1;
+                Console.WriteLine(numShort);
+            }
+            {
+                byte num1 = 4;
+                Console.WriteLine(num1);
+                // byte num2 = num1 + 5; //error
+                byte num2 = (byte)(num1 + 10);
+                Console.WriteLine(num2);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
