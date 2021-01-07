e3495c5d9f0d25d338b0a8b72210c04baf54d10e
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-07 23:44:17 +0400

    switch operator

--- CSharp_9/Switch/Program.cs
+++ CSharp_9/Switch/Program.cs
@@ -41,6 +41,7 @@ namespace Switch
                 Console.WriteLine("There are seven days on planet earth in a week");
             }
             #endregion
+            #region switch operator
             switch (dayIndex)
             {
                 case 1:
@@ -68,6 +69,87 @@ namespace Switch
                     Console.WriteLine("There are seven days on planet earth in a week");
                     break;
             }
+            #endregion
+            #region example 2
+            Console.WriteLine("Enter the level");
+            int level = Convert.ToInt32(Console.ReadLine());
+            if (level==1)
+            {
+                Console.WriteLine("NPC generation");
+                Console.WriteLine("NPC generation");
+            }
+            else if (level == 2)
+            {
+                Console.WriteLine("NPC generation");
+                Console.WriteLine("NPC generation");
+                Console.WriteLine("NPC generation");
+                Console.WriteLine("NPC generation");
+            }
+            else if (level==3)
+            {
+                Console.WriteLine("NPC generation");
+                Console.WriteLine("NPC generation");
+                Console.WriteLine("NPC generation");
+                Console.WriteLine("NPC generation");
+                Console.WriteLine("NPC generation");
+            }
+            else
+            {
+                Console.WriteLine("You did not enter the level correctly");
+            }
+
+            switch (level)
+            {
+                case 1:
+                    Console.WriteLine("NPC generation");
+                    Console.WriteLine("NPC generation");
+                    break;
+                case 2:
+                    Console.WriteLine("NPC generation");
+                    Console.WriteLine("NPC generation");
+                    Console.WriteLine("NPC generation");
+                    Console.WriteLine("NPC generation");
+                    break;
+                case 3:
+                    Console.WriteLine("NPC generation");
+                    Console.WriteLine("NPC generation");
+                    Console.WriteLine("NPC generation");
+                    Console.WriteLine("NPC generation");
+                    Console.WriteLine("NPC generation");
+                    break;
+                default:
+                    Console.WriteLine("You did not enter the level correctly");
+                    break;
+            }
+            #endregion
+            #region example 3
+            Random rand = new Random();
+
+            int value = rand.Next(16);
+
+            if (value>=0 && value<7)
+            {
+                Console.WriteLine("0 <= value <7");
+            }
+            else if (value>=7 && value<16)
+            {
+                Console.WriteLine("7 <= value <16");
+            }
+
+            switch (value)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:Console.WriteLine("0 <= value <7 "); break;
+                default:
+                    Console.WriteLine(" 7 <= value <16");
+                    break;
+            }
+
+            #endregion
         }
     }
 }
