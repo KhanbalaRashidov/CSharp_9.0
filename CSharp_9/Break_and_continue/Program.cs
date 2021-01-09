01524923aad96c4ba19d4fc9746682d610d4dbbf
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-09 23:10:41 +0400

    break and continue

---
+++ CSharp_9/Break_and_continue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Break_and_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<--Multiplication table-->");
+
+            for (int i = 1; i <=10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i}x{j}={i*j} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
