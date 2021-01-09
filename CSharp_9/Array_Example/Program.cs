9589b7f41d087eb5bfed084e097d7ef4e73b58b0
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-09 05:22:36 +0400

    array example

---
+++ CSharp_9/Array_Example/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace Array_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region example 1
+
+            Random rand = new Random();
+            Console.WriteLine("Enter the size of the array...");
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rand.Next(-10, 11);
+                Console.Write($"{numbers[i]}  ");
+            }
+            Console.WriteLine($"\n{"  <-Elements of an array->",28}");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]}  ");
+            }
+            Console.ReadKey();
+
+            #endregion
+
+            #region Example 2
+            Console.Clear();
+            Console.WriteLine("Enter the size of the array...");
+            int m = int.Parse(Console.ReadLine());
+
+            int[] numbers2 = new int[m];
+            int eventElement = 0;
+            for (int i = 0; i < numbers2.Length; i++)
+            {
+                numbers2[i] = rand.Next(21);
+                Console.Write($"{numbers2[i]}  ");
+                if (numbers2[i] % 2 == 0)
+                {
+                    eventElement++;
+                }
+            }
+            Console.WriteLine($"\n Event element={eventElement}");
+            Console.ReadKey();
+
+            #endregion
+            Console.Clear();
+            #region Example 3
+            int[] numbers_10 = new int[10];
+            for (int i = 0; i < numbers_10.Length; i++)
+            {
+                numbers_10[i] = rand.Next(20);
+                Console.Write($"{numbers_10[i]}  ");
+            }
+            Console.WriteLine("\nThe item you are looking for");
+            Console.WriteLine("Enter the find element...");
+            int element = int.Parse(Console.ReadLine());
+            int find = 0;
+            for (int i = 0; i < numbers_10.Length; i++)
+            {
+                if (numbers_10[i] == element)
+                {
+                    find++;
+                }
+            }
+            Console.WriteLine($"Element {element} to {find} times.");
+
+            #endregion
+            #region Example 4
+            Console.Clear();
+            Console.WriteLine("Enter the size of the array...");
+            int size_of = int.Parse(Console.ReadLine());
+            int[] array = new int[size_of];
+            Console.WriteLine("Array element");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 2);
+                Console.Write($"{array[i]} ");
+            }
+
+            int head = 0;
+            int tail = array.Length - 1;
+
+            bool flag = true;
+
+            while (head < tail)
+            {
+                if (flag)
+                {
+                    if (array[head] == 1)
+                    {
+                        flag = false;
+                    }
+                    else head++;
+                }
+                else
+                {
+                    if (array[tail] == 0)
+                    {
+                        flag = true;
+                    }
+                    else tail--;
+                }
+
+                if (array[head]==1 && array[tail]==0)
+                {
+                    int temp = array[head];
+                    array[head] = array[tail];
+                    array[tail] = temp;
+
+                    head++;
+                    tail--;
+                }
+
+            }
+
+            Console.WriteLine("\nChange array:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            #endregion
+        }
+    }
+}
