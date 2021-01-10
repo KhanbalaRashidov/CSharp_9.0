cd04387acbefd168233ceb3b639ca2d45771c846
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-10 04:53:09 +0400

    matrix3D and matrix4D example

--- CSharp_9/Array2D_Example/Program.cs
+++ CSharp_9/Array2D_Example/Program.cs
@@ -7,50 +7,50 @@ namespace Array2D_Example
         static void Main(string[] args)
         {
             #region Example 1
-            //int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            //int[,] b = new int[3, 3];
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[,] b = new int[3, 3];
 
-            //int counter = 0;
-            //for (int i = 0; i < b.GetLength(0); i++)
-            //{
-            //    for (int j = 0; j < b.GetLength(1); j++)
-            //    {
-            //        b[i, j] = a[counter++];
-            //        Console.Write($"{b[i,j]}\t");
-            //    }
-            //    Console.WriteLine("\n\n");
-            //}
-            //Console.ReadKey();
+            int counter = 0;
+            for (int i = 0; i < b.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    b[i, j] = a[counter++];
+                    Console.Write($"{b[i, j]}\t");
+                }
+                Console.WriteLine("\n\n");
+            }
+            Console.ReadKey();
             #endregion
             #region Example 2
-            //Random random = new Random();
-            //double[,] z = new double[3, 3];
-            //Console.WriteLine($"{"<-- Array z -->",20}");
-            //for (int i = 0; i < z.GetLength(0); i++)
-            //{
-            //    for (int j = 0; j < z.GetLength(1); j++)
-            //    {
-            //        z[i, j] = random.Next(1, 10);
-            //        Console.Write($"{z[i,j],6}");
-            //    }
-            //    Console.WriteLine();
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine($"{"Array z[3,3] after",20}");
-            //Console.WriteLine();
-            //for (int i = 0; i < z.GetLength(0); i++)
-            //{
-            //    double div = z[i, i];
-            //    for (int j = 0; j < z.GetLength(1); j++)
-            //    {
-            //        z[i, j] /= div;
-            //        Console.Write($"{z[i, j]:F2}\t");
-            //    }
-            //    Console.WriteLine("\n\n");
-            //}
+            Random random = new Random();
+            double[,] z = new double[3, 3];
+            Console.WriteLine($"{"<-- Array z -->",20}");
+            for (int i = 0; i < z.GetLength(0); i++)
+            {
+                for (int j = 0; j < z.GetLength(1); j++)
+                {
+                    z[i, j] = random.Next(1, 10);
+                    Console.Write($"{z[i, j],6}");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine($"{"Array z[3,3] after",20}");
+            Console.WriteLine();
+            for (int i = 0; i < z.GetLength(0); i++)
+            {
+                double div = z[i, i];
+                for (int j = 0; j < z.GetLength(1); j++)
+                {
+                    z[i, j] /= div;
+                    Console.Write($"{z[i, j]:F2}\t");
+                }
+                Console.WriteLine("\n\n");
+            }
             #endregion
             #region
-            Random random = new Random();
+            
             int[,] array = new int[4, 4];
             Console.WriteLine($"{"<---array[4,4]--->",25}");
             Console.WriteLine();
---
+++ CSharp_9/Array3D_Example/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Array3D_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] matrix3D = new int[3, 4, 5];
+
+            Console.WriteLine($"matrix3D.Rank={matrix3D.Rank}");
+
+            Console.WriteLine($"matrix3D.Length={matrix3D.Length}");
+            Console.WriteLine($"matrix3D.GetLangth(0)={matrix3D.GetLength(0)}");
+            Console.WriteLine($"matrix3D.GetLangth(1)={matrix3D.GetLength(1)}");
+            Console.WriteLine($"matrix3D.GetLangth(2)={matrix3D.GetLength(2)}");
+
+            Random random = new Random();
+            for (int i = 0; i < matrix3D.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix3D.GetLength(1); j++)
+                {
+                    for (int k = 0; k < matrix3D.GetLength(2); k++)
+                    {
+                        matrix3D[i, j, k] = random.Next(1, 10);
+                        Console.Write($"{matrix3D[i,j,k]}\t");
+                    }
+                    Console.WriteLine("\n");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("\n\n");
+            int[,,,] matrix4D = new int[3, 4, 5, 6];
+            Console.WriteLine($"matrix3D.Rank={matrix4D.Rank}");
+
+            Console.WriteLine($"matrix4D.Length={matrix4D.Length}");
+            Console.WriteLine($"matrix4D.GetLangth(0)={matrix4D.GetLength(0)}");
+            Console.WriteLine($"matrix4D.GetLangth(1)={matrix4D.GetLength(1)}");
+            Console.WriteLine($"matrix4D.GetLangth(2)={matrix4D.GetLength(2)}");
+            Console.WriteLine($"matrix4D.GetLangth(3)={matrix4D.GetLength(3)}");
+
+            Console.WriteLine("\n<--- matrix4D --->");
+            for (int i = 0; i < matrix4D.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix4D.GetLength(1); j++)
+                {
+                    for (int k = 0; k < matrix4D.GetLength(2); k++)
+                    {
+                        for (int m = 0; m < matrix4D.GetLength(3); m++)
+                        {
+                            matrix4D[i, j, k, m] = random.Next(1, 10);
+                            Console.Write($"{matrix4D[i,j,k,m]}\t");
+                        }
+                        Console.WriteLine("\n");
+                    }
+                    Console.WriteLine("\n");
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
