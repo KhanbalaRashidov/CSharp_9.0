493cb34f07d03da057fc97149b60ea18d9d08263
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-12 03:20:53 +0400

    array indices and ranges

--- CSharp_9/Array_Example/Program.cs
+++ CSharp_9/Array_Example/Program.cs
@@ -146,6 +146,38 @@ namespace Array_Example
                 Console.WriteLine($"{words[^i]}\t");
             }
             Console.ForegroundColor = ConsoleColor.Green;
+
+            string[] quickBrownFox = words[1..4];
+            foreach (var word in quickBrownFox)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+
+            string[] lazyDog = words[^2..^0];
+            foreach (var word in lazyDog)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+
+            Console.WriteLine("allWords,firstPhrase,lastPhrase...");
+            string[] allWords = words[..]; // contains "The" through "dog".
+            string[] firstPhrase = words[..4]; // contains "The" through "fox"
+            string[] lastPhrase = words[6..]; // contains "the, "lazy" and "dog"
+            foreach (var word in allWords)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+            foreach (var word in firstPhrase)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+            foreach (var word in lastPhrase)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+
+            Index the = ^3;
+            Console.WriteLine(words[the]);
+            Range phrase = 1..4;
+            string[] text = words[phrase];
+            foreach (var word in text)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
             #endregion
         }
     }
