a0c4f5ee46a35b70834a364a709e0065c4d21a9a
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-13 23:30:25 +0400

    Method Example

--- CSharp_9/Method_Example/Program.cs
+++ CSharp_9/Method_Example/Program.cs
@@ -4,9 +4,31 @@ namespace Method_Example
 {
     class Program
     {
+        static void Hi()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void Pause()
+        {
+            Console.ReadKey();
+        }
+        static void ApplauseAndOvations()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < rand.Next(30,51); i++)
+            {
+                int posX = rand.Next(Console.WindowWidth - 5);
+                int posY = rand.Next(Console.WindowHeight - 1);
+                Console.SetCursorPosition(posX, posY+1);
+                Console.Write(rand.Next(2)==0?"Yeahh":"Yuuu");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Hi();
+            ApplauseAndOvations();
+            Pause();
+
         }
     }
 }
