da13035e88571f395fc19af124aded3b98b96bcf
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-14 19:15:54 +0400

    Method return type example

--- CSharp_9/Method_returnType/Program.cs
+++ CSharp_9/Method_returnType/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.IO;
 using static System.Net.Mime.MediaTypeNames;
 namespace Method_returnType
 {
@@ -8,31 +10,49 @@ namespace Method_returnType
         {
             return Guid.NewGuid().ToString();
         }
+        
         static string GetRandomHi()
         {
             Random random = new Random();
             string text = string.Empty;
             switch (random.Next(4))
             {
-                case 0: text= $"Hello";break;
-                case 1:text= $"Hi"; break;
-                case 2: text=$"Good morning";break;
-                case 3:text= $"Good night";break;
+                case 0: text+= $"Hello";break;
+                case 1:text+= $"Hi"; break;
+                case 2: text+=$"Good morning";break;
+                case 3:text+= $"Good night";break;
                 default:
-                    text= $"See you";
+                    text+= $"See you";
                     break;
             }
             return text;
         }
         static string GetPath()
         {
-            return AppDomain.CurrentDomain.BaseDirectory;
+            return Assembly.GetEntryAssembly().Location;
+            //return AppDomain.CurrentDomain.BaseDirectory;
         }
+      
+
+        static int Max(int value1,int value2)
+        {
+            return value1 > value2 ? value1 : value2;
+        }
+        
         static void Main(string[] args)
         {
-            GetRandomHi();
-            GetRandomString();
-            //GetPath();
+            #region Example1
+            Console.WriteLine("Method no parametr");
+            string result = GetRandomString(); Console.WriteLine(result);
+            result = GetRandomHi(); Console.WriteLine(result);
+            result = GetPath(); Console.WriteLine(result);
+            #endregion
+            #region Example2
+            
+            Console.WriteLine(Max(14,16));
+
+            
+            #endregion
         }
     }
 }
---
+++ CSharp_9/Method_returnType_Example/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace Method_returnType_Example
+{
+    class Program
+    {
+        static string GetRandomString(int count)
+        {
+            return Guid.NewGuid().ToString().Substring(0, count);
+        }
+        static string GetPath(bool fileName)
+        {
+            return fileName ? Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) : Assembly.GetExecutingAssembly().Location;
+        }
+        static int[] Inverse(int[] array)
+        {
+            int[] inverseArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                inverseArray[i] = -array[i];
+            }
+            return inverseArray;
+        }
+        static void Pause(int interval)
+        {
+            Thread.Sleep(1000 * interval);
+        }
+
+        static void Fill(int[] arr)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(10);
+            }
+        }
+        static void Print(int [] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+        }
+        static int MaxElement(int [] arr)
+        {
+            //return arr.Max();
+            int max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+        static int MinElement(int[] arr)
+        {
+            // return arr.Min();
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (min>arr[i])
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            #region Example1 
+            Console.WriteLine("Method parametr");
+            Console.WriteLine(GetRandomString(14));
+            Console.WriteLine(GetRandomString(16));
+            Console.WriteLine(GetRandomString(9));
+            Console.WriteLine(GetPath(true));
+            Console.WriteLine(GetPath(false));
+
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (var item in array)
+            {
+                Console.Write($"{item}\t");
+            }
+            
+            Console.WriteLine("\n\nInverse Array");
+            int[] inverseArray = Inverse(array);
+            foreach (var item in inverseArray)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+            #endregion
+            #region Example 2
+            Random random = new Random();
+            int n = random.Next(10, 21);
+            int[] arrA = new int[n];
+            Fill(arrA);
+            Console.WriteLine();
+            Print(arrA);
+            Console.WriteLine($"max element arrA={MaxElement(arrA)}");
+            Console.WriteLine($"Min element arrA={MinElement(arrA)}");
+            Console.WriteLine();
+            int m = random.Next(10, 21);
+            int[] arrB = new int[m];
+            Pause(5);
+
+            Fill(arrB);
+            Console.WriteLine();
+            Print(arrB);
+            Console.WriteLine($"max element arrB={MaxElement(arrB)}");
+            Console.WriteLine($"min element arrB={MinElement(arrB)}");
+            #endregion
+
+        }
+    }
+}
