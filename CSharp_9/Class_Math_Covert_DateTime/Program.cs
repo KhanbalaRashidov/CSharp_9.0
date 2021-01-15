8a6cad5a5bed992ca9ed89adbd5c34bf15ad61dd
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-15 23:29:02 +0400

    Class Math Convert Date Time

--- CSharp_9/Class_Math_Covert_DateTime/Program.cs
+++ CSharp_9/Class_Math_Covert_DateTime/Program.cs
@@ -6,7 +6,45 @@ namespace Class_Math_Covert_DateTime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Convert class
+            //byte
+            //sbyte
+            //int
+            //char
+            //bool
+            //double
+            //long
+            //float
+            //decimal
+            //DateTime
+            //uint
+            //short
+            //ushort
+            //ulong
+            //string
+
+            int i = Convert.ToByte("30");
+            int j = byte.Parse("30");
+            Console.WriteLine($"i={i}  j={j}");
+
+            i = Convert.ToByte(16.9);
+            Console.WriteLine($"i={i}");
+            j = Convert.ToByte(9.16);
+            Console.WriteLine($"j={j}");
+
+            i = Convert.ToByte(16.9f);
+            Console.WriteLine($"i={i}");
+            j = Convert.ToByte(9.16f);
+            Console.WriteLine($"j={j}");
+
+            i = Convert.ToByte(16.9m);
+            Console.WriteLine($"i={i}");
+            j = Convert.ToByte(9.16m);
+            Console.WriteLine($"j={j}");
+
+
+
+            #endregion
         }
     }
 }
