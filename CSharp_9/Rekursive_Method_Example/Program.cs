6e1a3933b53264e55f7948f3b9c5f308093f3f5f
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-15 23:07:37 +0400

    Rekursive method example

--- CSharp_9/Rekursive_Method_Example/Program.cs
+++ CSharp_9/Rekursive_Method_Example/Program.cs
@@ -47,6 +47,43 @@ namespace Rekursive_Method_Example
                 Find(set, newWord, n + 1);
             }
         }
+
+        static void Print(char[,] Map)
+        {
+            for (int i = 0; i < Map.GetLength(0); i++)
+            {
+                for (int j = 0; j < Map.GetLength(1); j++)
+                {
+                    Console.Write(Map[i, j] == '.' ? ' ' : Map[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+        }
+
+        static void ToColor(char[,] Map, int PosX, int PosY)
+        {
+            //Console.ForegroundColor = ConsoleColor.Cyan;
+            if (Map[PosX, PosY] == '.')
+            {
+                Map[PosX, PosY] = '+';              
+                ToColor(Map, PosX - 1, PosY);      
+                ToColor(Map, PosX, PosY + 1);       
+                ToColor(Map, PosX + 1, PosY);       
+                ToColor(Map, PosX, PosY - 1);       
+            }
+        }
+        static int F(int n)
+        {
+            Console.WriteLine($"F({n})");
+            return (n > 2) ? F(n - 1) + G(n - 2) : n;
+        }
+        static int G(int n)
+        {
+            Console.WriteLine($"G({n})");
+          return  (n > 2) ? G(n - 1) + F(n - 2) : n + 1;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Rekursive Method");
@@ -63,6 +100,31 @@ namespace Rekursive_Method_Example
             char[] alphabit = { 'a', 'b','c','d','e','f'};
             char[] word = new char[3];
             Find(alphabit, word, 0);
+
+            //ToColor
+
+            char[,] map = new char[,]
+             {
+                {'.','+','+','.','+','+','.', },
+                {'+','.','.','+','.','.','+', },
+                {'+','.','.','.','.','.','+', },
+                {'.','+','.','.','.','+','.', },
+                {'.','.','+','.','+','.','.', },
+                {'.','.','.','+','.','.','.', }
+            };
+
+            Print(map);
+            Console.WriteLine();
+            ToColor(map, 3, 3);
+            Print(map);
+
+            //F(n) G(n)
+
+            Console.WriteLine(F(7));
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine(F(14));
+            
         }
     }
 }
