3e9ad76cbe6c47a81da8206efb740968817a3322
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-15 23:13:17 +0400

    Class Math Convert Date Time

---
+++ CSharp_9/Class_Math_Covert_DateTime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class_Math_Covert_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
