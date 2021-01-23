3f8a6c65d75901e0a2a36f78593253d9621792ac
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-24 00:00:11 +0400

    Example

---
+++ CSharp_9/Encapsulation_OOP/Computer1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_OOP
+{
+    public struct Computer1
+    {
+        public string Title { get; set; }
+
+        
+        public Computer1(string Title)
+        {
+            this.Title = Title;
+        }
+
+        #region Методы Open/Close
+
+        
+        public void ActivationBIOS() { Console.WriteLine($"{this.Title} | Activation BIOS..."); }
+
+       
+        public void ActivationOS() { Console.WriteLine($"{this.Title} | Activation OS..."); }
+
+        
+        public void Greeting() { Console.WriteLine($"{this.Title} | Hello..."); }
+
+        public void DeactivationBIOS() { Console.WriteLine($"{this.Title} | Deactivation BIOS..."); }
+
+        
+        public void DeactivationOS() { Console.WriteLine($"{this.Title} | Deactivation OS..."); }
+
+       
+        public void Parting() { Console.WriteLine($"{this.Title} | Bye..."); }
+
+        #endregion
+
+       
+        public void Calculation()
+        {
+            Console.WriteLine($"\n{this.Title} | Calculation...\n");
+        }
+    }
+}
---
+++ CSharp_9/Encapsulation_OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Encapsulation_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Encapsulation\n");
+
+            #region Computer1
+            Computer1 computer1 = new Computer1("Computer_1");
+            computer1.ActivationBIOS();
+            computer1.ActivationOS();
+            computer1.Calculation();
+            computer1.DeactivationBIOS();
+            computer1.DeactivationOS();
+            computer1.Parting();
+            #endregion
+
+            #region Computer2
+            Computer2 computer2 = new Computer2("Computer2");
+            computer2.PowerOff();
+            computer2.PowerOn();
+            #endregion 
+        }
+    }
+}
