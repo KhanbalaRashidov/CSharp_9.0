cd2c52e909c7db0890c900891814c53cc1b19156
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-26 23:02:28 +0400

    Dictionary

---
+++ CSharp_9/Dictionary_Collection/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Dictionary\n");
+
+            //Represents a collection of keys and values.
+            Dictionary<string, string> pairs = new Dictionary<string, string>();
+            //Method  Add -->  Adds the specified key and value to the dictionary.
+            pairs.Add("Muellim", "Teacher");
+            pairs.Add("Yoxla", "Check");
+            pairs.Add("Komputer", "Computer");
+            pairs.Add("Avtomobil", "Car");
+
+            //Struct KeyValuePair<string, string> -> Defines a key/value pair that can be set or retrieved.
+            Console.WriteLine("\npairs");
+            foreach (KeyValuePair<string, string> e in pairs) Console.WriteLine($"{e} ");      // Output of all pairs      
+
+            //Prop [] --> Gets or sets the value associated with the specified key.
+            Console.WriteLine($"\n\npairs[\"Muellim\"] = {pairs["Muellim"]}"); // Teacher
+
+            //Prop Keys --> Gets a collection containing the keys in the Dictionary<TKey,TValue>.
+            Console.WriteLine("\npairs.Keys");
+            foreach (var e in pairs.Keys) Console.Write($"{e} ");      // Outputting all keys
+
+            //Prop Values --> Gets a collection containing the values in the Dictionary<TKey,TValue>.
+            Console.WriteLine("\n\npairs.Values");
+            foreach (var e in pairs.Values) Console.Write($"{e} ");      // Outputting all keys
+
+            //Method ContainsKey --> Determines whether the Dictionary<TKey,TValue> contains the specified key.
+            Console.WriteLine(pairs.ContainsKey("Muellim"));
+
+            //Method Remove --> Removes the value with the specified key from the Dictionary<TKey,TValue>.
+            Console.WriteLine("\n\nDelete \"Muellim\"");
+
+            pairs.Remove("Muellim");  // Delete by key
+
+            Console.WriteLine("\npairs");
+            foreach (KeyValuePair<string, string> e in pairs) Console.WriteLine($"{e} ");      // Output of all pairs   
+
+            Console.WriteLine("\n");
+
+            //Prop Count -->  Gets the number of key/value pairs contained in the Dictionary<TKey,TValue>.
+            Console.WriteLine($"Элементов в словаре: {pairs.Count}"); // 3
+            
+            //Method Clear --> Removes all keys and values from the Dictionary<TKey,TValue>.
+            pairs.Clear();             // Clear dictionary
+            Console.WriteLine("pairs.Clear(); completed");
+            Console.WriteLine($"Elements in the dictionary: {pairs.Count}"); // 0
+        }
+    }
+}
