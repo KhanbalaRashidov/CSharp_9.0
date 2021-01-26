aa39030426fa787d5546305502c9d3e851d65462
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-26 23:58:34 +0400

    HashSet<T>

---
+++ CSharp_9/HashSet_Collection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashSet_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
