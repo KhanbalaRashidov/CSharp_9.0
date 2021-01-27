97e9155e4b619e048bfe6e802e52150d21db9aab
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-27 22:09:19 +0400

    Dictionary Example

---
+++ CSharp_9/Dictionary_Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            Random r = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                list.Add(r.Next(20));
+            }
+
+            var dictionary = new Dictionary<int, int>();
+
+            foreach (var e in list)
+            {
+                if (!dictionary.ContainsKey(e)) dictionary.Add(e, 0);
+                dictionary[e]++;
+            }
+
+            foreach (KeyValuePair<int, int> e in dictionary)
+            {
+                Console.WriteLine($"{e.Key,3} meets {e.Value,4} .");
+            }
+        }
+    }
+}
--- CSharp_9/HashSet_Collection/Program.cs
+++ CSharp_9/HashSet_Collection/Program.cs
@@ -35,13 +35,34 @@ namespace HashSet_Collection
                 Console.WriteLine($"{item}");
             }
             Console.WriteLine();
-            // IntersectWith - Modifies the current HashSet<T> object to contain only elements that are present in that object and in the specified collection.
 
+            // IntersectWith - Modifies the current HashSet<T> object to contain only elements that are present in that object and in the specified collection.
+            Console.WriteLine("IntersecWith method");
+            hashset.IntersectWith(new int[] { 1, 2, 3, 3, 4, 4, 5, 6 });
+            foreach (var item in hashset)
+            {
+                Console.WriteLine($"{item}");
+            }
             // ExceptWith -Removes all elements in the specified collection from the current HashSet<T> object.
+            Console.WriteLine("ExceptWith method");
+            hashset.ExceptWith(new int[] { 1, 2, 3, 4 });
+            foreach (var item in hashset)
+            {
+                Console.WriteLine($"{item}");
+            }
             // IsSubsutOf - Determines whether a HashSet<T> object is a subset of the specified collection.
+            Console.WriteLine("IsSubsutOf method");
+            Console.WriteLine(hashset.IsSubsetOf(new int[] { 5,6}));
             // IsSuperSetOf - Determines whether a HashSet<T> object is a superset of the specified collection.
+            Console.WriteLine("IsSuperSetOf method");
+            Console.WriteLine(hashset.IsSupersetOf(new int[] { 1,2,3,4,5}));
             // IsProperSubsetOf - Determines whether a HashSet<T> object is a proper subset of the specified collection.
+            Console.WriteLine("IsProperySubsetOf method");
+            Console.WriteLine(hashset.IsProperSubsetOf(new int[] { 1,2,3,4,5}));
             // IsProperSupersetOf - Determines whether a HashSet<T> object is a proper superset of the specified collection.
+            Console.WriteLine("IsPropertySupersetOf");
+            Console.WriteLine(hashset.IsProperSupersetOf(new int[] { 1,2,3,4}));
+
         }
     }
 }
