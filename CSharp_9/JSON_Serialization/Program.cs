2460e3f012e029b294614d9a90f27e965799e938
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-01-29 23:56:31 +0400

    Serialization JSON

---
+++ CSharp_9/JSON_Serialization/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+
+namespace JSON_Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Json Serialization\n");
+
+            Worker worker = new Worker("Khanbala", "Rashidov", "Developer", "IT", 2000);
+            string json = JsonConvert.SerializeObject(worker);
+            File.WriteAllText("_khanbala.json", json);
+
+            var jsonDeserialize = File.ReadAllText("_khanbala.json");
+            var deserializeWorker = JsonConvert.DeserializeObject<Worker>(jsonDeserialize);
+            Console.WriteLine(deserializeWorker.Print());
+
+            List<Worker> list = new List<Worker>();
+            for (int i = 1; i <= 5; i++)
+            {
+                list.Add(new Worker($"FirstName{i}", $"LastName{i}", $"Position{i}", $"Position{i}", 1000 + i));
+            }
+
+            string jsonWorkerList = JsonConvert.SerializeObject(list);
+            File.WriteAllText("_WorkerList.json",jsonWorkerList);
+
+            var workerList = File.ReadAllText("_WorkerList.json");
+            list = JsonConvert.DeserializeObject<List<Worker>>(workerList);
+            Console.WriteLine();
+
+            foreach (var item in list)
+            {
+                Console.Write($"{item.FirstName}\t");
+            }
+        }
+    }
+}
