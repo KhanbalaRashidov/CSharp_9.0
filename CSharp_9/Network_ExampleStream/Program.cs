a50ed861d7d91d1e393e035a185a9a0395f22634
KhanbalaRashidov <50279392+xanishreshidov@users.noreply.github.com>
2021-02-02 20:49:12 +0400

    Example Network

---
+++ CSharp_9/Network_ExampleStream/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.IO;
+namespace Network_ExampleStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            string url = @"https://algs4.cs.princeton.edu/14analysis/1Mints.txt";
+            using (Stream sr = client.OpenRead(url))
+            {
+                using (StreamReader reader=new StreamReader(sr))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(reader.ReadToEnd());
+                    }
+                }
+            }
+
+            Console.WriteLine("Txt file read!");
+            Console.ReadKey();
+        }
+    }
+}
